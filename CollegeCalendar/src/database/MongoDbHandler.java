9c6bd3166802fb26b6b736548895431bd65c6ae7
abhig1997 <gupta257@purdue.edu>
2018-09-16 15:25:59 -0400

    added method to get all users from database

--- CollegeCalendar/src/database/MongoDbHandler.java
+++ CollegeCalendar/src/database/MongoDbHandler.java
@@ -10,51 +10,76 @@ import com.mongodb.*;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
+import javax.crypto.Cipher;
+
+/**
+ * 
+ * @author Abhi Gupta
+ *
+ */
+
 
 public class MongoDbHandler {
 	
 	private static MongoClient mongoClient;
 	private static MongoDatabase database;
 	
-//	private static MongoCollection<Document> collection;
 	
 	public MongoDbHandler() {
 		mongoClient = new MongoClient("localhost", 27017);
 		database = mongoClient.getDatabase("408testdb");
-//		collection = database.getCollection("testData");
 		
 	}
 	
 	
+	/*
+	 * Main method for the database handler
+	 */
 	public static void main(String[] args) {
 		
 		MongoDbHandler handler = new MongoDbHandler();
 		
 		handler.insertUser("testUser", "testPassword");
 		
-//		MongoClient mongoClient = new MongoClient("localhost", 27017);
-		
-//		DB database = mongoClient.getDB("408testdb");
-//		DBCollection collection = database.getCollection("users");
-//		System.out.println(collection);
-		
-		
-//		List<String> dbNames = mongoClient.getDatabaseNames();
-//		System.out.println(dbNames);
-		
-		
-//		mongoClient.close();
+		handler.getAllUsers();
 	}
 	
 	
+	/*
+	 * Inserts a user with the given username and password in the database, under
+	 * users collection
+	 * 
+	 * @param username the username of the new user
+	 * @param password the password of the new user
+	 */
 	void insertUser(String username, String password) {
 		MongoCollection<Document> collection = database.getCollection("users");
-		Document newUser = new Document("_id", 1)
-				.append("username", username)
+		Document newUser = new Document("username", username)
 				.append("password", password);
 		
 		collection.insertOne(newUser);
 	}
+	
+	
+	/*
+	 * Get all the users currently in the database
+	 */
+	void getAllUsers() {
+		MongoCollection<Document> collection = database.getCollection("users");
+		collection.find().forEach(printBlock);
+	}
+	
+	
+	
+	/*
+	 * Used for printing returns from a collection
+	 */
+	Block<Document> printBlock = new Block<Document>() {
+	       @Override
+	       public void apply(final Document document) {
+	           System.out.println(document.toJson());
+	       }
+	};
 	
 	
 }
