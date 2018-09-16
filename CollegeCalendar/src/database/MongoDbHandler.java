2c5eb485492b2aaf9aaa3b5f36968419bd84684d
abhig1997 <gupta257@purdue.edu>
2018-09-15 21:00:47 -0400

    code is able to view databases in local instance of mongo

--- CollegeCalendar/src/database/MongoDbHandler.java
+++ CollegeCalendar/src/database/MongoDbHandler.java
@@ -1,13 +1,23 @@
 package database;
 
 
+import java.util.List;
+
 import com.mongodb.*;
+import com.mongodb.client.MongoDatabase;
 
 
 public class MongoDbHandler {
 	public static void main(String[] args) {
 		@SuppressWarnings({ "unused", "resource" })
-		MongoClient mongoclient = new MongoClient("localhost", 27017);
+		MongoClient mongoClient = new MongoClient("localhost", 27017);
 		System.out.println("we lit");
+		
+		MongoDatabase dbs = mongoClient.getDatabase("simplease");
+		System.out.println("Connect to database successfully");
+		System.out.println("Database Name: "+dbs.getName());
+		
+		List<String> dbNames = mongoClient.getDatabaseNames();
+		System.out.println(dbNames);
 	}
 }
