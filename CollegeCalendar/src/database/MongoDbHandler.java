4305a05fcebac80dc92cf2eaa415e118f31c2fcb
abhig1997 <gupta257@purdue.edu>
2018-09-16 21:11:50 -0400

    finished basic password encryption and decryption

--- CollegeCalendar/src/database/MongoDbHandler.java
+++ CollegeCalendar/src/database/MongoDbHandler.java
@@ -12,6 +12,7 @@ import static com.mongodb.client.model.Filters.*;
 
 import com.mongodb.client.MongoDatabase;
 
+import javax.crypto.spec.SecretKeySpec;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.AlgorithmParameters;
@@ -19,7 +20,10 @@ import java.security.GeneralSecurityException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.util.Base64;
+
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.IvParameterSpec;
@@ -39,27 +43,31 @@ public class MongoDbHandler {
 	
 	private static MongoClient mongoClient;
 	private static MongoDatabase database;
+	private String encryptionPassword;
 	
 	
 	public MongoDbHandler() {
 		mongoClient = new MongoClient("localhost", 27017);
 		database = mongoClient.getDatabase("408testdb");
-		
 	}
 	
 	
 	/*
 	 * Main method for the database handler
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		MongoDbHandler handler = new MongoDbHandler();
 		
-		handler.insertUser("testUser", "testPassword");
+//		handler.insertUser("testUser", "testPassword");
 		
-//		handler.getAllUsers();
-		
-//		handler.getUserByUsername("testUser");
+		boolean result = handler.isValidUser("testUser", "testPassword");
+		if (result) {
+			System.out.println("Successful login");
+		}
+		else {
+			System.out.println("Unsuccessful login");
+		}
 	}
 	
 	
@@ -70,23 +78,108 @@ public class MongoDbHandler {
 	 * @param username the username of the new user
 	 * @param password the password of the new user
 	 */
-	void insertUser(String username, String password) {
+	void insertUser(String username, String password) throws Exception {
 		// need to check if a user already exists with the given user name
 		Document oldUser = getUserByUsername(username);
 		if (oldUser != null) {
-//			System.out.println("User already exists");
 			throw new java.lang.Error("User already exists");
 		}
+		
+		String encryptionPassword = "testing_password";
+		
+		// do some password encryption stuff
+		byte[] salt = new String("12345678").getBytes();
+		
+		// using a small-ish number for speed
+		int iterationCount = 400;
+		
+		int keyLength = 128;
+		SecretKeySpec key = createSecretKey(encryptionPassword.toCharArray(), salt, iterationCount, keyLength);
+		
+		String encryptedPassword = encrypt(password, key); // this is the password that should be stored in the database
 		
 		
 		MongoCollection<Document> collection = database.getCollection("users");
 		Document newUser = new Document("username", username)
-				.append("password", password);
+				.append("password", encryptedPassword);
 		
 		collection.insertOne(newUser);
 	}
 	
+	boolean isValidUser(String username, String password) throws GeneralSecurityException, IOException {
+		// get the user from the database with the given username
+		Document user = getUserByUsername(username);
+		
+		if (user == null) {
+			// the username is not in the database
+			System.out.println("Invalid username");
+			return false;
+		}
+		
+		
+		String db_username = (String) user.get("username");
+		String db_password = (String) user.get("password");
+		
+		
+		// decrypt db_password
+		// do some password encryption stuff
+		byte[] salt = new String("12345678").getBytes();
+		
+		// using a small-ish number for speed
+		int iterationCount = 400;
+		
+		int keyLength = 128;
+		SecretKeySpec key = createSecretKey("testing_password".toCharArray(), salt, iterationCount, keyLength);
+		String decryptedPassword = decrypt(db_password, key);
+		
+		if (password.equals(decryptedPassword)) {
+			return true;
+		}
+		else {
+			return false;
+		}		
+	}
 	
+	
+	private String decrypt(String string, SecretKeySpec key) throws GeneralSecurityException, IOException{
+		String iv = string.split(":")[0];
+        String property = string.split(":")[1];
+        Cipher pbeCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        pbeCipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(base64Decode(iv)));
+        return new String(pbeCipher.doFinal(base64Decode(property)), "UTF-8");
+	}
+
+
+	private byte[] base64Decode(String property) {
+		// TODO Auto-generated method stub
+		return Base64.getDecoder().decode(property);
+	}
+
+
+	private String encrypt(String property, SecretKeySpec key) throws GeneralSecurityException, UnsupportedEncodingException {
+		Cipher pbeCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        pbeCipher.init(Cipher.ENCRYPT_MODE, key);
+        AlgorithmParameters parameters = pbeCipher.getParameters();
+        IvParameterSpec ivParameterSpec = parameters.getParameterSpec(IvParameterSpec.class);
+        byte[] cryptoText = pbeCipher.doFinal(property.getBytes("UTF-8"));
+        byte[] iv = ivParameterSpec.getIV();
+        return base64Encode(iv) + ":" + base64Encode(cryptoText);
+	}
+
+
+	private String base64Encode(byte[] bytes) {
+		return Base64.getEncoder().encodeToString(bytes);
+	}
+
+
+	private SecretKeySpec createSecretKey(char[] password, byte[] salt, int iterationCount, int keyLength) throws NoSuchAlgorithmException, InvalidKeySpecException {
+		SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+        PBEKeySpec keySpec = new PBEKeySpec(password, salt, iterationCount, keyLength);
+        SecretKey keyTmp = keyFactory.generateSecret(keySpec);
+        return new SecretKeySpec(keyTmp.getEncoded(), "AES");
+	}
+
+
 	/*
 	 * Get all the users currently in the database
 	 */
