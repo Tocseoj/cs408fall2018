0dc7d02941fd13962c528761c73ecfc171cc2881
abhig1997 <gupta257@purdue.edu>
2018-09-18 20:41:38 -0400

    setup for junit

---
+++ CollegeCalendar/tests/DbUserTests.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+
+public class DbUserTests {
+	@Test
+	
+   public void testAdd() {
+      String str = "Junit is working fine";
+      assertEquals("Junit is working fine",str);
+   }
+}
