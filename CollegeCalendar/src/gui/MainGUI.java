0505dd9013cc2adf3af6943c244571039753bc0b
Joseph Marshall <marsha87@purdue.edu>
2018-09-21 17:20:11 -0400

    Reformatted

--- CollegeCalendar/src/gui/MainGUI.java
+++ CollegeCalendar/src/gui/MainGUI.java
@@ -19,90 +19,90 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class MainGUI extends Application{
-	private Controller ic = new Controller();
-	private ArrayList<MeetingGO> meetingList;
-	private ArrayList<HomeworkGO> homeworkList;
-	private ArrayList<GenericGO> genericList;	
-	private ArrayList<ExamGO> examList;
-	private ArrayList<ClassGO> classList;
+	
+	private Controller ic = new Controller();				// Variable to talk to Database
+	
+	private ArrayList<GenericGO> genericList;				// Event list to display on GUI
 
-	private String userName = "testUser";
+	private String userName = "testUser";					// Sprint 2 add user account
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		try {
-			meetingList = ic.getAllMeetings(userName);
-			homeworkList = ic.getAllHomeworks(userName);
-			genericList = ic.getAllGenerics(userName);
-			examList = ic.getAllExams(userName);
-			classList = ic.getAllClasses(userName);
 
+		try {
+
+			genericList = ic.getAllGenerics(userName);		// Fill List with objects from database
+
+			// Generate window and show
 			Scene scene = fxTest();
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			
-			primaryStage.setTitle("JavaFX Test");
+			primaryStage.setTitle("Good Shit");
 			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
+	
+	// Generates window and elements and add Event handlers to elements
 	public Scene fxTest() {
+		
+		// Setup main window with width 1280 and height 840
 		BorderPane root = new BorderPane();
-		Scene scene = new Scene(root,500,400);
+		Scene scene = new Scene(root, 1280, 840);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 
+		// Add rounded rectangle (bounds buttons)
 		Rectangle rect = new Rectangle();
 		rect.setX(0.0); 
 		rect.setY(0.0); 
 		rect.setWidth(150.0); 
-		rect.setHeight(150.0d); 
-		
+		rect.setHeight(150.0d);
 		rect.setArcWidth(30.0);
 		rect.setArcHeight(20.0);
+		rect.setFill(Color.ALICEBLUE);
 		
+		// Add Blue Rectangle to top center
 		Rectangle rect2 = new Rectangle(400.0, 100.0, Color.BLUEVIOLET);
 		rect2.setArcWidth(30.0);
 		rect2.setArcHeight(20.0);
 		BorderPane.setAlignment(rect2, Pos.TOP_CENTER);
 		
-		Rectangle rect3 = new Rectangle(400.0, 100.0, Color.DARKORANGE);
+		// Add Rectangle to bottom center?
+		Rectangle rect3 = new Rectangle(400.0, 100.0, Color.CORAL);
 		rect3.setArcWidth(30.0);
 		rect3.setArcHeight(20.0);
 		BorderPane.setAlignment(rect3, Pos.TOP_CENTER);
-		
-		VBox buttonGroup = new VBox();
-		
-		MeetingGO cgo = new MeetingGO("5ba3b32818f6310f6c7d8cde","haaa");
 
+		// Add Buttons
 		Button storeDataButton = new Button("Store in Database");
-		buttonGroup.getChildren().add(storeDataButton);
-		storeDataButton.setUserData(cgo);
 		Button getDataButton = new Button("Get All Events");
+		// Add to button Group
+		VBox buttonGroup = new VBox();
+		buttonGroup.getChildren().add(storeDataButton);
 		buttonGroup.getChildren().add(getDataButton);
 		
-		
-		//Eventhandler<MouseEvent>
-		storeDataButton.setOnAction(new EventHandler<ActionEvent> () {
-			public void handle(ActionEvent e) {
-				MeetingGO asd = (MeetingGO)storeDataButton.getUserData();
-				
-				System.out.println(ic.getMeetingName(asd));
-			}
-		});
-		
-		getDataButton.setOnAction(new EventHandler<ActionEvent> () {
-			public void handle(ActionEvent e) {
-				System.out.println("Getting");
-			}
-		});
-		
-		//root.getChildren().add(rect);
+		// Add shit to scene
+		root.getChildren().add(rect);
 		root.setCenter(buttonGroup);
 		root.setTop(rect2);
 		root.setBottom(rect3);
-		
 		scene.setFill(Color.WHITE);
+		
+		
+		// "Store in Database" Event handler
+		storeDataButton.setOnAction(new EventHandler<ActionEvent> () {
+			public void handle(ActionEvent e) {				
+				System.out.println("Saved to database. TODO");
+			}
+		});
+		// "Get All Events" Event handler
+		getDataButton.setOnAction(new EventHandler<ActionEvent> () {
+			public void handle(ActionEvent e) {
+				System.out.println("Getting from database? TODO");
+			}
+		});
 		
 		return scene;
 	}
