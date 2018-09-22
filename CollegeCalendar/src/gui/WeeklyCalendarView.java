ff0063296dcbe1363528116cd4caea232388f166
abhig1997 <gupta257@purdue.edu>
2018-09-22 16:17:10 -0400

    weekly view is displaying, still need to fix the computing dates properly

--- CollegeCalendar/src/gui/WeeklyCalendarView.java
+++ CollegeCalendar/src/gui/WeeklyCalendarView.java
@@ -22,45 +22,62 @@ import javafx.scene.text.*;
 
 import java.time.LocalDate;
 import java.time.YearMonth;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
-public class MonthlyCalendarView {
+public class WeeklyCalendarView {
+
+	// Class variables
 	private ArrayList<AnchorPaneNode> allCalendarDays = new ArrayList<>(35); // 35 anchorpanenodes, each one represents a day
 	
 	
 	private VBox view;
 	private Text calendarView;
 	
-
+	private Date date; // today's date
+	
+	private int day; // todays day
+	
+	private int month; // the current month
+	
+	private int year; // the current year
 	
 	private YearMonth currentYearAndMonth;
 	
-	private Text monthlyCalendarTitle; // the title of the monthly calendar
+	private Text weeklyCalendarTitle; // the title of the weekly calendar
 										// currently being viewed
 	
 	
-	public MonthlyCalendarView(YearMonth currMonth) {
-		this.currentYearAndMonth = currMonth;
+	
+	/*
+	 * Constructor
+	 */
+	public WeeklyCalendarView(YearMonth yearAndMonth) {
+		this.currentYearAndMonth = yearAndMonth;
 		
-		//Make a grid pane for the calendar
-		GridPane monthlyCalendar = new GridPane();
-		monthlyCalendar.setPrefSize(600, 400);
 		
-		monthlyCalendar.setGridLinesVisible(true);
+		GridPane weeklyCalendar = new GridPane();
+		double prefWidth = 600;
+		double prefHeight = 200;
+		weeklyCalendar.setPrefSize(prefWidth, prefHeight);
+		
+		weeklyCalendar.setGridLinesVisible(true);
 		
 		// add all the days/nodes for the monthly calendar
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < 1; i++) {
 			for (int j = 0; j < 7; j++) {
 				AnchorPaneNode currDay = new AnchorPaneNode();
 				currDay.setPrefSize(200,200);
-				monthlyCalendar.add(currDay, j, i);
+				weeklyCalendar.add(currDay, j, i);
 				allCalendarDays.add(currDay);
 			}
 		}
 		
 		GridPane dayNamesGrid = new GridPane();
 		dayNamesGrid.setPrefWidth(600);
-		Integer col = 0;
+		Integer col = 0; // track the column we're editing
 		
 		Text[] dayLabels = new Text[] {
 				new Text("Sunday"),
@@ -79,93 +96,126 @@ public class MonthlyCalendarView {
 			double val = 5.0;
 			pane.setBottomAnchor(currDayName, val);
 			pane.getChildren().add(currDayName);
-			dayNamesGrid.add(pane, col++, 0); // 0 because we want the first row
+			dayNamesGrid.add(pane, col, 0); // 0 because we want the first row
 											// to have the day labels
-			
+			col++;
 		}
-		
-		monthlyCalendarTitle = new Text();
-		
+
+		weeklyCalendarTitle = new Text();
 		
 		Button userOptions = new Button("User Options");
 		
 		// Create a button to go back a month
-		Button prevMonth = new Button("Prev Month");
+		Button prevWeek = new Button("Prev Week");
 		// assign the action to go back a month to this button
-		prevMonth.setOnAction(e -> viewPreviousMonth());
+		prevWeek.setOnAction(e -> viewPreviousWeek());
 		
 		// do the same for a next month button
-		Button nextMonth = new Button("Next Month");
+		Button nextWeek = new Button("Next Week");
 		// assign onaction
-		nextMonth.setOnAction(e -> viewNextMonth());
+		nextWeek.setOnAction(e -> viewNextWeek());
 		
 		
-		HBox title = new HBox(prevMonth, monthlyCalendarTitle, nextMonth);
+		HBox title = new HBox(prevWeek, weeklyCalendarTitle, nextWeek);
 		
 		title.setAlignment(Pos.BASELINE_CENTER); // center align the title bar
 		
+		
+		// create a new date object, with today's date
+		this.date = new Date();
+		// convert to LocalDate to use the getters
+		LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		
+//		int day = cal.get(Calendar.DAY_OF_WEEK); // get todays day
+		int day   = localDate.getDayOfMonth();
+		
+		// set class variable to today's day
+		this.day = day;
+		
+		// get the current month
+		int month = localDate.getMonthValue();
+		// set the class variable
+		this.month = month;
+		
+		int year = localDate.getYear();
+		this.year = year;
+		
+		System.out.println("this month is " + year);
+
+		
+		
+//		System.out.println("val of date is: " + day);
+		
 		computeCalendar(currentYearAndMonth);
 		
-		view = new VBox(title, dayNamesGrid, monthlyCalendar); // the calendar view
+		view = new VBox(title, dayNamesGrid, weeklyCalendar); // the calendar view
+		
+		
 	}
 	
 	
-	
 	private void computeCalendar(YearMonth yearAndMonth) {
-		// TODO Auto-generated method stub
-		// TODO Auto-generated method stub
-		// Get the date we want to start with on the calendar
-		LocalDate currDate = LocalDate.of(yearAndMonth.getYear(), yearAndMonth.getMonthValue(), 1);
+
 		
-		// need to compute the first sunday before the currDate
+		
+		
+		// create a var to hold today's date
+		LocalDate currDate = LocalDate.of(yearAndMonth.getYear(), yearAndMonth.getMonthValue(), this.day);
+
+		// need to compute the first sunday before the currDate, calendar starts on sunday
 		while (!currDate.getDayOfWeek().toString().equals("SUNDAY")) {
 			// subtract a day from currDate
 			currDate = currDate.minusDays(1);
 		}
-		
-		
+
+		// currDate should be the first Sunday of the month that the user requested
+
 		// compute the day numbers for the calendar
 		for (AnchorPaneNode node : allCalendarDays) {
 			if (node.getChildren().size() != 0) {
 				node.getChildren().remove(0);
 			}
-			
+
 			Text text = new Text(String.valueOf(currDate.getDayOfMonth()));
 			node.setDate(currDate); // set the date of the node
-			
+
 			double anchorVal = 5.0;
 			node.setTopAnchor(text, anchorVal);
 			node.setLeftAnchor(text,  anchorVal);
 			node.getChildren().add(text);
-			
-			
+
+
 			// increment the currDate by 1
 			currDate = currDate.plusDays(1);
 		}
-		
-		
+
+
 		// compute the title of the calendar
 		String currMonth = yearAndMonth.getMonth().toString();
 		currMonth = currMonth.substring(0, 1).toUpperCase() + currMonth.substring(1).toLowerCase();
 		String currYear = String.valueOf(yearAndMonth.getYear());
-		monthlyCalendarTitle.setText(currMonth + ", " + currYear);
+		weeklyCalendarTitle.setText(currMonth + ", " + currYear);
 	}
 
 
-
-	private void viewPreviousMonth() {
+	/*
+	 * 
+	 */
+	private void viewNextWeek() {
 		// TODO Auto-generated method stub
-		currentYearAndMonth = currentYearAndMonth.minusMonths(1); // increment the month
-		computeCalendar(currentYearAndMonth); // re-compute the days of the calendar
-	}
-	
-	
-	private void viewNextMonth()
-	{
 		currentYearAndMonth = currentYearAndMonth.plusMonths(1); // increment the month
 		computeCalendar(currentYearAndMonth); // re-compute the days of the calendar
 	}
 	
+	
+	
+	/*
+	 * Rewind the weekly calendar view by a week
+	 */
+	private void viewPreviousWeek() {
+		currentYearAndMonth = currentYearAndMonth.minusMonths(1); // increment the month
+		computeCalendar(currentYearAndMonth); // re-compute the days of the calendar
+	}
 	
 	
 	/*
@@ -183,9 +233,10 @@ public class MonthlyCalendarView {
 	}
 	
 	/*
-	 * 
+	 * Setter for calendar days
 	 */
 	public void setAllCalendarDays(ArrayList<AnchorPaneNode> allCalendarDays) {
 		this.allCalendarDays = allCalendarDays;
 	}
+	
 }
