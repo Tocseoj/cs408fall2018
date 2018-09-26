9857a4d9f07c2c23d8d5011752cafbff06805fdd
Joseph Marshall <marsha87@purdue.edu>
2018-09-25 20:24:39 -0400

    added test add event method

--- CollegeCalendar/src/gui/JoeGUI.java
+++ CollegeCalendar/src/gui/JoeGUI.java
@@ -5,17 +5,25 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.TextStyle;
 import java.time.temporal.TemporalAdjusters;
+import java.util.ArrayList;
 import java.util.Locale;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.StackPane;
@@ -29,6 +37,8 @@ public class JoeGUI extends Application {
 	LocalDate date;
 	LocalDate monthBeingViewed;
 	
+	static ArrayList<EventGO> events;
+	
 	// Change these 4 lines to edit grid and add/subtract rows or columns
 	ColumnConstraints[] columnList = new ColumnConstraints[7];
 	RowConstraints[] rowList = new RowConstraints[8];
@@ -37,12 +47,15 @@ public class JoeGUI extends Application {
 	
 	Stage primaryStage;
 	GridPane gridpane = new GridPane();
-	Button[][] buttonChildren = new Button[6][7];
+//	Button[][] buttonChildren = new Button[6][7];
 	Label monthYear = new Label();
 	DateTimeFormatter monthYearFormatter = DateTimeFormatter.ofPattern("MMMM yyyy");
 	
 		
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
+		events = getAllEvents();
+//		System.out.println(events);
+		
 		launch(args);
 	}
 
@@ -130,6 +143,14 @@ public class JoeGUI extends Application {
 		    }
 		});
 		gridpane.add(view, 6, 0);
+		Button addEvent = new Button("Add Event");
+		addEvent.getStyleClass().add("month-change-button");
+		addEvent.setOnAction(new EventHandler<ActionEvent>() {
+		    @Override public void handle(ActionEvent e) {
+		    	addEventDialog();
+		    }
+		});
+		gridpane.add(addEvent, 6, 7);
 		// Day of week Labels
 	    for (int c = 0; c < 7; c++) {
 	    	Label l = new Label();
@@ -168,12 +189,26 @@ public class JoeGUI extends Application {
 		    }
 		};
 		
+		ArrayList<Node> toRemove = new ArrayList<>();
+		for (Node node : gridpane.getChildren()) {
+			int r = GridPane.getRowIndex(node);
+			int c = GridPane.getColumnIndex(node);
+			if (calendarRows[0] <= r && r <= calendarRows[1]) {
+				if (calendarColumns[0] <= c && c <= calendarColumns[1]) {
+					toRemove.add(node);
+				}
+			}
+		}
+		for (Node node : toRemove) {
+			gridpane.getChildren().remove(node);
+		}
+		
 		for (int r = calendarRows[0]; r <= calendarRows[1]; r++) {
 	    	for (int c = calendarColumns[0]; c <= calendarColumns[1]; c++) {
-	    		if (buttonChildren[r - calendarRows[0]][c - calendarColumns[0]] != null) {
-	    			gridpane.getChildren().remove(buttonChildren[r - calendarRows[0]][c - calendarColumns[0]]);
-	    			buttonChildren[r - calendarRows[0]][c - calendarColumns[0]] = null;
-	    		}
+//	    		if (buttonChildren[r - calendarRows[0]][c - calendarColumns[0]] != null) {
+//	    			gridpane.getChildren().remove(buttonChildren[r - calendarRows[0]][c - calendarColumns[0]]);
+//	    			buttonChildren[r - calendarRows[0]][c - calendarColumns[0]] = null;
+//	    		}
 	    		int dayOfMonth = ((r - calendarRows[0]) * 7) + ((c - calendarColumns[0]) + 1) - (firstOfMonth.getDayOfWeek().getValue() == 7 ? 0 : firstOfMonth.getDayOfWeek().getValue());
 	    		if (dayOfMonth >= 1 && dayOfMonth <= lastOfMonth.getDayOfMonth()) {
 		    		Button b = new Button(String.valueOf(dayOfMonth));
@@ -184,12 +219,32 @@ public class JoeGUI extends Application {
 		    		
 		    		b.setOnAction(calendarDayEvent);
 		    		gridpane.add(b, c, r);
-		    		buttonChildren[r - calendarRows[0]][c - calendarColumns[0]] = b;
+		    		
+		    		// Add events
+		    		ArrayList<EventGO> daysEvents = getEventOnDay(firstOfMonth.plusDays(dayOfMonth - 1));
+		    		for (int i = 0; i < daysEvents.size(); i++) {
+		    			EventGO e = daysEvents.get(i);
+		    			Button event = new Button(e.getTitle());
+		    			event.getStyleClass().add("calendar-day-events");
+		    			gridpane.add(event, c, r);
+		    		}
+		    		
+//		    		buttonChildren[r - calendarRows[0]][c - calendarColumns[0]] = b;
 	    		}
 	    	}
 	    }
 		
 		monthYear.setText(monthBeingViewed.format(monthYearFormatter));
+	}
+	
+	private ArrayList<EventGO> getEventOnDay(LocalDate day) {
+		ArrayList<EventGO> e = new ArrayList<>();;
+		for (int i = 0; i < events.size(); i++) {
+			if (events.get(i).getDate().equals(day)) {
+				e.add(events.get(i));
+			}
+		}
+		return e;
 	}
 	
 	private void viewDay(String day) {
@@ -216,5 +271,124 @@ public class JoeGUI extends Application {
         Scene dialogScene = new Scene(scroll, width, height);
         dialog.setScene(dialogScene);
         dialog.show();
+	}
+	
+	private void addEventDialog() {
+		// Setup Dialog
+		final Stage dialog = new Stage();
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        dialog.initOwner(primaryStage);
+        // Setup Container
+        int width = 400;
+        int height = 600;
+        ScrollPane scrollWrapper = new ScrollPane();
+        scrollWrapper.setPrefSize(width, height);
+        VBox containerPane = new VBox();
+//        containerPane.setPrefSize(width, height);
+        
+//        VBox column1 = new VBox();
+//        VBox column2 = new VBox();
+//        containerPane.getChildren().add(column1);
+//        containerPane.getChildren().add(column2);
+        
+        // Add components
+//        TextField eventName = new TextField();
+//        containerPane.getChildren().add(eventName);
+        
+        containerPane.getChildren().add(new Label("Event Name"));
+        containerPane.getChildren().add(new TextField());
+        containerPane.getChildren().add(new Label("Event Date"));
+        containerPane.getChildren().add(new DatePicker());
+        containerPane.getChildren().add(new Label("Event Time"));
+        containerPane.getChildren().add(new TextField());
+        containerPane.getChildren().add(new Label("Duration"));
+        containerPane.getChildren().add(new TextField());
+        
+        containerPane.getChildren().add(new Label("Priority"));
+        containerPane.getChildren().add(new TextField());
+        
+        Label endRepeatL = new Label("End Repeat");
+        TextField endRepeat = new TextField();
+        Label repeatDaysL = new Label("Repeat on Days");
+        HBox repeatDays = new HBox();
+        for (int i = 0; i < 7; i++) {
+        	VBox vb = new VBox();
+        	vb.getChildren().add(new CheckBox());
+        	Label l = new Label();
+	    	DayOfWeek day = DayOfWeek.of(((i == 0) ? 7 : i));
+	    	l.setText(day.getDisplayName(TextStyle.SHORT_STANDALONE, Locale.US));
+        	vb.getChildren().add(l);
+        	repeatDays.getChildren().add(vb);
+        }
+        
+        CheckBox repeat = new CheckBox();
+        repeat.setOnAction(new EventHandler<ActionEvent>() {
+		    @Override public void handle(ActionEvent e) {
+		    	CheckBox self = (CheckBox)e.getSource();
+		    	if (self.isSelected()) {
+		    		int index = containerPane.getChildren().indexOf(self);
+		    		containerPane.getChildren().add(index + 1, repeatDays);
+		    		containerPane.getChildren().add(index + 1, repeatDaysL); 
+		    		containerPane.getChildren().add(index + 1, endRepeat);
+		    		containerPane.getChildren().add(index + 1, endRepeatL); 
+		    	} else {
+		    		containerPane.getChildren().remove(endRepeatL);
+		            containerPane.getChildren().remove(endRepeat);
+		            containerPane.getChildren().remove(repeatDays);
+		    		containerPane.getChildren().remove(repeatDaysL); 
+		    	}
+		    }
+		});
+        
+//        containerPane.getChildren().add(endRepeatL);
+//        containerPane.getChildren().add(endRepeat);
+        
+        Label notificationOffsetL = new Label("(Hours:)Minutes before to notify");
+        TextField notificationOffset = new TextField();
+        
+        containerPane.getChildren().add(new Label("Notify Me"));
+        CheckBox notify = new CheckBox();
+        notify.setOnAction(new EventHandler<ActionEvent>() {
+		    @Override public void handle(ActionEvent e) {
+		    	CheckBox self = (CheckBox)e.getSource();
+		    	if (self.isSelected()) {
+		    		int index = containerPane.getChildren().indexOf(self);
+		    		containerPane.getChildren().add(index + 1, notificationOffset);
+		    		containerPane.getChildren().add(index + 1, notificationOffsetL);
+		    	} else {
+		    		containerPane.getChildren().remove(notificationOffsetL);
+		            containerPane.getChildren().remove(notificationOffset);
+		    	}
+		    }
+		});
+        containerPane.getChildren().add(notify);
+        containerPane.getChildren().add(new Label("Repeat"));
+        containerPane.getChildren().add(repeat);
+        Button submit = new Button("Submit");
+        submit.setOnAction(new EventHandler<ActionEvent>() {
+		    @Override public void handle(ActionEvent e) {
+		    	System.out.println("Hello");
+		    	EventGO event = new EventGO(""+0, "New Event", LocalDate.now(), "joe");
+		    	events.add(event);
+		    	redrawCalendarView();
+		    }
+		});
+        containerPane.getChildren().add(submit);
+        
+        // Add container to scene to stage
+        scrollWrapper.setContent(containerPane);
+        Scene dialogScene = new Scene(scrollWrapper, width, height);
+        dialog.setScene(dialogScene);
+        dialog.show();
+	}
+	
+	private static ArrayList<EventGO> getAllEvents() {
+		// TODO Get real events
+		ArrayList<EventGO> list = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			EventGO e = new EventGO(""+i, "Test Event", LocalDate.now().plusDays(i + 1), "joe");
+			list.add(e);
+		}
+		return list;
 	}
 }
